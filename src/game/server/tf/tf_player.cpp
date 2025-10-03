1ff2f12ff8f4c5e60523dd73c42b76bab5e40573
bignitwit <thomas.baratta@outlook.com>
2025-10-03 19:28:28 -0400

    - Adjust frenzy wrench meter fill

--- src/game/server/tf/tf_player.cpp
+++ src/game/server/tf/tf_player.cpp
@@ -8757,7 +8757,7 @@ void HandleRageGain( CTFPlayer *pPlayer, unsigned int iRequiredBuffFlags, float 
 	{
 		if ( pPlayer->IsPlayerClass( TF_CLASS_ENGINEER ) && ( kRageBuffFlag_OnDamageDealt & iRequiredBuffFlags ) )
 		{
-			pPlayer->m_Shared.ModifyRage( 2.f * flDamage / fInverseRageGainScale);
+			pPlayer->m_Shared.ModifyRage(0.44 * (flDamage / fInverseRageGainScale));
 		}
 		else if ( pPlayer->IsPlayerClass( TF_CLASS_HEAVYWEAPONS ) && ( kRageBuffFlag_OnDamageDealt & iRequiredBuffFlags ) )
 		{
